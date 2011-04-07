cd6e338ba730a64a8d17eb6cce7377ffc7e68cc9
Solomon Desktop <solomon.kahn@gmail.com>
2011-04-07 17:26:07 -0500

    Finished adding home and about pages without formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 TodoApp::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  match '/about', :to => 'pages#about'
+
+  root :to => "pages#home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

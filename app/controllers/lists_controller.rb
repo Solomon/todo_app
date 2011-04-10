f97c84863870431fc77a8179f2bb0b030c73a2e2
Solomon Desktop <solomon.kahn@gmail.com>
2011-04-10 17:02:50 -0500

    Finished with basic list functionality

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,23 +1,60 @@
 class ListsController < ApplicationController
 
 	before_filter :authenticate_user!
-	before_filter :authorized_user, :only => [:show, :destroy]
+	before_filter :authorized_user, :only => [:show, :destroy, :edit]
 
 
 	def create
 		@list = current_user.lists.build(params[:list])
 		if @list.save
 			flash[:success] = "List Created"
-			redirect_to root_path
+			redirect_to lists_path
 		else
 			flash[:notice] = "Could not create list"
-			redirect_to root_path
+			redirect_to new_list_path
 		end
 	end
 
 	def index
 		@lists = current_user.lists.paginate(:page => params[:page])
 		@title = "Lists"
+	end
+
+	def new
+		@title = "New List"
+		@list = List.new		
+	end
+
+	def show
+		@title = "List"
+		@list = List.find(params[:id])
+	end
+
+	def edit
+		@title = "Edit List"
+		@list = List.find(params[:id])
+	end
+
+	def update
+		@list = List.find(params[:id])
+		if @list.update_attributes(params[:list])
+			flash[:success] = "List updated"
+			redirect_to list_path(@list)
+		else
+			@title = "Edit List"
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@list = List.find(params[:id])
+		if @list.destroy
+			flash[:notice] = "List Deleted"
+			redirect_to lists_path
+		else
+			flash[:error] = "Could not delete"
+			redirect_to lists_path
+		end
 	end
 
 
--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -34,6 +34,49 @@ describe ListsController do
 				end.should_not change(List, :count)
 			end
 		end
+
+		describe "success" do
+
+			before(:each) do
+				@attr = {:name => "New List", :description => "a new to do list"}
+			end
+
+			it "should create a list" do
+				lambda do
+					post :create, :list => @attr
+				end.should change(List, :count).by(1)
+			end
+		end
+
+	end
+
+	describe "GET 'new'" do
+
+		describe "if not signed in" do
+
+			it "should redirect to sign in page" do
+				get :new
+				response.should redirect_to(new_user_session_path)
+			end
+
+		end
+
+		describe "if signed in" do
+
+			before(:each) do
+				@user = sign_in(Factory(:user))
+			end
+
+			it "should be successful" do
+				get :new
+				response.should be_success
+			end
+
+			it "should have the right title" do
+				get :new
+				response.should have_selector("title", :content => "New List")
+			end
+		end
 	end
 
 
